61fe0f8ea2783419d1b94dcb1764a9de1fccbec9
Dovydas Lozys <reduktorius@gmail.com>
2022-01-28 22:59:53 +0000

    clean up routes moved profiles into their own file, still not functional

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,23 +1,11 @@
-import express from "express";
-import {getAllEvents, getUpcomingEvents, createEvent, deleteEvent, getEvent, updateEvent} from "../controllers/events.js";
-
-// import {
-//   getProfileHistory,
-//   deleteProfileHistory,
-// } from "../controllers/profiles";
+import express from 'express';
+import {getAllEvents, getUpcomingEvents, createEvent, deleteEvent, getEvent, updateEvent} from '../controllers/events.js';
 
 const eventRouter = express.Router();
 
-eventRouter.route("/").get(getAllEvents);
-eventRouter.route("/host-event").post(createEvent);
-eventRouter.route("/upcomingevents/:count").get(getUpcomingEvents);
-eventRouter.route("/:id").delete(deleteEvent).get(getEvent).patch(updateEvent);
-
-//user profile stuff
-
-// eventRouter
-//   .route("/profile/:name")
-//   .get(getProfileHistory)
-//   .delete(deleteProfileHistory);
+eventRouter.route('/').get(getAllEvents);
+eventRouter.route('/host-event').post(createEvent);
+eventRouter.route('/upcomingevents/:count').get(getUpcomingEvents);
+eventRouter.route('/:id').get(getEvent).patch(updateEvent).delete(deleteEvent);
 
 export {eventRouter};
--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -1,5 +1,6 @@
-// import express from "express";
-// import {
-//   getProfileHistory,
-//   deleteProfileHistory,
-// } from "../controllers/profiles";
+import express from 'express';
+import {getProfileHistory, deleteProfileHistory} from '../controllers/profiles.js';
+
+const profileRouter = express.Router();
+
+profileRouter.route('/profile/:name').get(getProfileHistory).delete(deleteProfileHistory);
