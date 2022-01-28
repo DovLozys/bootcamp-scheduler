5709584c9ab415b5463777c5c55603c32e7111de
Dovydas Lozys <reduktorius@gmail.com>
2022-01-28 22:44:46 +0000

    selectData.js script restored to original purpose of debugging only

--- server/db/scripts/selectData.js
+++ server/db/scripts/selectData.js
@@ -3,7 +3,7 @@ import {pool} from '../connection.js';
 async function selectData() {
   const data = await pool.query('SELECT * FROM events ORDER BY id ASC;');
 
-  return data;
+  console.log(data);
 }
 
-export {selectData};
+selectData();
