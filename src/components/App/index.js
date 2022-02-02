608a7449a2819f9bd962e71da390ca7fce4f5df8
Dovydas Lozys <reduktorius@gmail.com>
2022-02-02 19:49:26 +0000

    todo: started on refactoring routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,17 @@
 import {Routes, Route} from 'react-router-dom';
 import './App.css';
-import HomePage from '../HomePage';
+// import HomePage from '../HomePage';
 import HostEventPage from '../HostEventPage';
 import {DisplayAllEvents} from '../DisplayAllEvents';
 import ProfilePage from '../ProfilePage';
 
+import MainContent from "../MainContent";
+import Navbar from "../Navbar";
+
 function App() {
     return (
         <Routes>
-            <Route path='/' element={<HomePage />} />
+            <Route path='/' element={<><Navbar /><MainContent /></>}/>
             <Route path='/host-event' element={<HostEventPage />} />
             <Route path='/all-events' element={<DisplayAllEvents />} />
             <Route path='/profile' element={<ProfilePage />} />
