9f3f15a3d5c7fe5c76625f9a29e8d7341619e5d6
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2023-03-07 18:59:50 +0000

    removes unnecessary css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,17 +5,15 @@ import HomePage from '../HomePage';
 import HostEventPage from '../HostEventPage';
 import ProfilePage from '../ProfilePage';
 
-import './App.css';
-
 function App() {
-    return (
-        <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/host-event' element={<HostEventPage />} />
-            <Route path='/all-events' element={<DisplayAllEvents />} />
-            <Route path='/profile' element={<ProfilePage />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/host-event" element={<HostEventPage />} />
+      <Route path="/all-events" element={<DisplayAllEvents />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
+  );
 }
 
 export { App };
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,13 +1,11 @@
 import MainContent from '../MainContent';
 import Navbar from '../Navbar';
 
-import './homepage.css';
-
 export default function HomePage() {
   return (
-    <main className="homepage-body">
+    <>
       <Navbar />
       <MainContent />
-    </main>
+    </>
   );
 }
